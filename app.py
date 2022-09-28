a342352dba328ef7b44acbe4f2cd1a8457bd559f
RitikKumawat <90177001+RitikKumawat@users.noreply.github.com>
2022-09-28 19:20:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 model = pickle.load(open('model.pkl','rb'))
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
 @app.route('/predict',methods=['POST'])
